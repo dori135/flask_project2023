368b6715f1f836945198653bc4d976d3b9f9716b
­지아현(인공지능대학 인공지능학과) <nolli77@ewhain.net>
2023-11-09 07:56:57 +0000

    그룹실습과제1

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
-import sys 
+from database import DBhandler
+import sys
+
 application = Flask(__name__)
+
+DB = DBhandler()
 
 @application.route("/")
 def hello():
@@ -33,20 +37,20 @@ def reg_item_submit():
     card = request.args.get("card")
     status = request.args.get("status")
     phone = request.args.get("phone")
-    
+
     print(name, seller, addr, email, category, card, status, phone)
     #return render_template("reg_item.html")
 
 @application.route("/submit_item_post", methods=['POST'])
 def reg_item_submit_post():
-    
-    image_file=request.files["file"]
+    image_file = request.files["file"]
     image_file.save("static/images/{}".format(image_file.filename))
-
-    data=request.form
-    return render_template("result.html", data=data,
-                           img_path="static/images/{}".format(image_file.filename))
-
+    data = request.form
+    DB.insert_item(data['name'], data, image_file.filename)
     
+    return render_template("submit_item_result.html", data=data, img_path=
+                           "static/images/{}".format(image_file.filename))
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
+    
