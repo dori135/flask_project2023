1f1c21b55c96b6ff30fbb1951f67a63791557365
­지아현(인공지능대학 인공지능학과) <nolli77@ewhain.net>
2023-11-16 08:44:02 +0000

    상품리스트 화면

--- application.py
+++ application.py
@@ -4,17 +4,45 @@ import hashlib
 import sys
 
 application = Flask(__name__)
-application.config["SECRET_KEY"] = "helloosp"
+application.config["SECRET_KEY"] = "helloosp" 
+# secret key: helloosp
 
 DB = DBhandler()
 
 @application.route("/")
 def hello():
-    return render_template("index.html")
+    # return render_template("index.html")
+    return redirect(url_for('view_list'))
 
 @application.route("/list")
 def view_list():
-    return render_template("list.html")
+    # return render_template("list.html")
+    page = request.args.get("page", 0, type=int)
+    per_page = 6
+    per_row = 3
+    row_count = int(per_page/per_row)
+    start_idx = per_page*page
+    end_idx = per_page*(page+1)
+    data = DB.get_items()
+    item_counts = len(data)
+    data = dict(list(data.items())[start_idx:end_idx])
+    tot_count = len(data)
+    for i in range(row_count):
+        if (i == row_count-1) and (tot_count % per_row != 0):
+            locals()['data_{}'.format(i)] = dict(list(data.items())
+                                                 [i*per_row:])
+        else:
+            locals()['data_{}'.format(i)] = dict(list(data.items())
+                                                 [i*per_row:(i+1)*per_row])
+    return render_template(
+        "list.html",
+        datas=data.items(),
+        row1=locals()['data_0'].items(),
+        row2=locals()['data_1'].items(),
+        limit=per_page,
+        page=page,
+        page_count=int((item_counts/per_page)+1),
+        total=item_counts)
 
 @application.route("/review")
 def view_review():
@@ -39,7 +67,7 @@ def reg_item_submit():
     status = request.args.get("status")
     intro = request.args.get("intro")
 
-    print(name, seller, addr, money, category, card, status, intro)
+    print(name, seller, addr, money, category, status, intro)
     #return render_template("reg_item.html")
 
 @application.route("/submit_item_post", methods=['POST'])
@@ -48,17 +76,32 @@ def reg_item_submit_post():
     image_file.save("static/images/{}".format(image_file.filename))
     data = request.form
     DB.insert_item(data['name'], data, image_file.filename)
-    
     return render_template("submit_item_result.html", data=data, img_path=
                            "static/images/{}".format(image_file.filename))
+
 
 @application.route("/login")
 def login():
     return render_template("login.html")
 
+
+@application.route("/login_confirm", methods=['POST'])
+def login_user():
+    id_=request.form['id']
+    pw=request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.find_user(id_,pw_hash):
+        session['id'] = id_
+        return redirect(url_for('view_list'))
+    else:
+        flash("Wrong ID or PW!")
+        return render_template("login.html")
+
+
 @application.route("/signup")
 def signup():
     return render_template("signup.html")
+
 
 @application.route("/signup_post", methods=['POST'])
 def register_user():
@@ -70,6 +113,20 @@ def register_user():
     else:
         flash("user id already exist!")
         return render_template("signup.html")
+
+
+@application.route("/logout")
+def logout_user():
+    session.clear()
+    return redirect(url_for('view_list'))
+
+
+@application.route("/view_detail/<name>/")
+def view_item_detail(name):
+    print("###name:", name)
+    data = DB.get_item_byname(str(name))
+    print("####data:", data)
+    return render_template("detail.html", name=name, data=data)
 
 
 if __name__ == "__main__":
