3edfaf54bc71bd76ba274165cde9151371fc8fb7
Jin0328 <chloe02.han@gmail.com>
2023-11-27 03:30:10 +0900

    리뷰 기능 구현

--- database.py
+++ database.py
@@ -4,8 +4,7 @@ import json
 class DBhandler:
     def __init__(self):
         with open('./authentication/Authentication/firebase_auth.json') as f:
-            config=json.load(f )
-        
+            config=json.load(f)
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
 
@@ -21,42 +20,61 @@ class DBhandler:
             return True
         else:
             return False
-        
-    def user_duplicate_check(self, id_string): 
+
+    def user_duplicate_check(self, id_string):
         users = self.db.child("user").get()
-        
-        print("users###",users.val())
-        if str(users.val()) == "None": # first registration 
+
+        print("users###", users.val())
+        if str(users.val()) == "None": # first registration
             return True
-        else: 
-            for res in users.each(): 
+        else:
+            for res in users.each():
                 value = res.val()
-            
-            if value['id'] == id_string: 
+            if value['id'] == id_string:
                 return False
             return True
+
     def find_user(self, id_, pw_):
         users = self.db.child("user").get()
-        target_value = []
+        target_value=[]
         for res in users.each():
             value = res.val()
-        
-        if value['id'] == id_ and value['pw'] == pw_:
-            return True
-        
+            if value['id'] == id_ and value['pw'] == pw_:
+                return True
         return False
-    
-    def insert_item(self, name, data, img_path): 
-        item_info ={
+
+    def insert_item(self, name, data, img_path):
+        item_info = {
             "seller": data['seller'],
             "addr": data['addr'],
-            "email": data['email'], 
-            "category": data['category'], 
-            # "card": data['card'],
-            "status": data['status'], 
-            "phone": data['phone'], 
+            "money": data['money'],
+            "category": data['category'],
+            "status": data['status'],
+            "intro": data['intro'],
             "img_path": img_path
-        } 
-        self.db.child("item").child(name).set(item_info) 
-        print(data,img_path)
+        }
+        self.db.child("item").child(name).set(item_info)
+        print(data, img_path)
+        return True
+
+    def get_items(self):
+        items = self.db.child("item").get().val()
+        return items
+
+    def get_item_byname(self, name):
+        items = self.db.child("item").get()
+        target_value = ""
+        print("###########", name)
+        for res in items.each():
+            key_value = res.key()
+            if key_value == name:
+                target_value = res.val()
+        return target_value
+    
+    def reg_review(self, data):
+        review_info={
+            "rate":data['reviewStar'],
+            "review": data['reviewContents']
+        }
+        self.db.child("review").child(data['name']).set(review_info)
         return True
